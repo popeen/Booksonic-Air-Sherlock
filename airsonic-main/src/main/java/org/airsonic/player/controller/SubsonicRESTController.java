87260153516a4fbabebe8c63d4e102012856f9a1
Patrik <patrik@ptjwebben.se>
2022-04-25 20:59:40 +0200

    Update getAlbumInfo to now show the Booksonic description as note. Could be useful for third party apps. Idea from popeen/Booksonic-Air/issues/62

--- airsonic-main/src/main/java/org/airsonic/player/controller/SubsonicRESTController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/SubsonicRESTController.java
@@ -2285,6 +2285,8 @@ public class SubsonicRESTController {
         AlbumNotes albumNotes = this.lastFmService.getAlbumNotes(mediaFile);
 
         AlbumInfo result = getAlbumInfoInternal(albumNotes);
+        result.setNotes(mediaFile.getDescription()); //Override the lastFm description with our own
+
         Response res = createResponse();
         res.setAlbumInfo(result);
         this.jaxbWriter.writeResponse(request, response, res);
